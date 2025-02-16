0052dc82947b7a150e99821cd08952a99066f700
NoMoreCakee <fokti.tamass@gmail.com>
2025-02-16 20:43:46 +0100

    cogs setup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import dotenv
 import asyncio
+import os
 
 from discord.ext import commands
 
@@ -11,10 +12,18 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=prefix, intents=intents)
 
+async def load_cogs():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
 @bot.event
 async def on_ready():
     print(f"Logged in as: {bot.user}!")
 
 async def main():
     async with bot:
+        await load_cogs()
         await bot.start(dotenv.get_key(".env", "API_KEY"))
+
+asyncio.run(main())
